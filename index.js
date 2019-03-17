816031984ff8c1f7ac8849cde9ee947b992c2893
Srini <srinivas.k947@gmail.com>
2019-03-17 17:58:31 +0800

    second express commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const app = require('express')();
+const express = require('express');
 
+const app = express();
+const morgan = require('morgan');
 const port = 5000;
-
-
-app.get('/',(req,res)=>{
-   
-  console.log(`request from ${req.url}`)
-  res.setHeader('Content-Type','text/html');
-  res.status(200).send('<html><body><h1>Hello, World!</h1></body></html>');
-})
-
+app.use(morgan('dev'));
+app.use(express.static(__dirname+'/public'));
 app.listen(port,() =>{
   console.log(`Server started and listening on port ${port}`);
+});
+
+app.get('/',(req,res)=>{   
+  res.setHeader('Content-Type','text/html');
+  res.status(200).send('<html><body><h1>Hello, World!</h1></body></html>');
 })
